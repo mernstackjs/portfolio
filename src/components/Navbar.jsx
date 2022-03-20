c96453a2d8cc1650a5dad9f3676924ce3bf1a4b6
Ahmed Abdirizaak <mernstack.js@gmail.com>
2022-03-20 14:18:05 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,146 @@
+import {
+  ListItemText,
+  MenuItem,
+  MenuList,
+  Toolbar,
+  Typography,
+  IconButton,
+  Drawer,
+  TextField,
+  InputAdornment,
+  Box,
+  List,
+  ListItemButton,
+  ListItemIcon,
+} from "@mui/material";
+import React, { useState } from "react";
+import { makeStyles } from "@mui/styles";
+import { Link } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import SearchIcon from "@mui/icons-material/Search";
+import WorkIcon from "@mui/icons-material/Work";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import RedeemIcon from "@mui/icons-material/Redeem";
+import SupportAgentIcon from "@mui/icons-material/SupportAgent";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import WebIcon from "@mui/icons-material/Web";
+const useStyles = makeStyles((theme) => {
+  return {
+    header: {
+      backgroundColor: "gray",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-between",
+      color: "white",
+      height: 48,
+      padding: "4px 30px",
+      width: "100%",
+    },
+    linkItem: {
+      color: "white",
+      textDecoration: "none",
+      [theme.breakpoints.down("sm")]: {
+        display: "none",
+      },
+    },
+    drw: {
+      [theme.breakpoints.up("sm")]: {
+        display: "none",
+      },
+    },
+
+    mobileMenu: {
+      [theme.breakpoints.up("sm")]: {
+        display: "none",
+      },
+    },
+  };
+});
+function Navbar() {
+  const [open, setOpen] = useState(false);
+  const classes = useStyles();
+  const navItem = [
+    { path: "/", item: "Home", icone: <DashboardIcon /> },
+    { path: "/Portfolio", item: "Portfolio", icone: <WebIcon /> },
+    { path: "/Work", item: "Work", icone: <WorkIcon /> },
+    { path: "/About", item: "About", icone: <RedeemIcon /> },
+    { path: "/fitness", item: "Fitness", icone: <FitnessCenterIcon /> },
+    { path: "/Contect", item: "Contect", icone: <SupportAgentIcon /> },
+  ];
+  return (
+    <header className={classes.header}>
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          width: "100%",
+        }}
+      >
+        <div>
+          <ListItemText>
+            <Typography sx={{ fontSize: "30px" }}>BrandName</Typography>
+          </ListItemText>
+        </div>
+        <MenuList sx={{ display: "flex" }}>
+          {navItem.map((items, index) => (
+            <Link className={classes.linkItem} key={index} to={items.path}>
+              <MenuItem sx={{ fontSize: "22px" }}>{items.item}</MenuItem>
+            </Link>
+          ))}
+        </MenuList>
+        <div className={classes.mobileMenu}>
+          <IconButton onClick={() => setOpen(!open)}>
+            {open ? (
+              <CloseIcon sx={{ color: "white", fontSize: "50px" }} />
+            ) : (
+              <MenuIcon sx={{ color: "white", fontSize: "50px" }} />
+            )}
+          </IconButton>
+          <Drawer
+            className={classes.drw}
+            open={open}
+            onClose={() => setOpen(false)}
+          >
+            <Box sx={{ padding: "20px", borderRadius: "none" }}>
+              <TextField
+                placeholder="serach here"
+                fullWidth
+                size="small"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <IconButton>
+                        <SearchIcon />
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <List>
+                {navItem.map((items) => (
+                  <Link style={{ textDecoration: "none" }} to={items.path}>
+                    <ListItemButton>
+                      <ListItemIcon>{items.icone}</ListItemIcon>
+                      <MenuItem
+                        sx={{
+                          color: "black",
+                          fontSize: "24px",
+                          textDecoration: "none",
+                        }}
+                      >
+                        {items.item}
+                      </MenuItem>
+                    </ListItemButton>
+                  </Link>
+                ))}
+              </List>
+            </Box>
+          </Drawer>
+        </div>
+      </Toolbar>
+    </header>
+  );
+}
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import { ThemeProvider } from "@mui/material";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { theme } from "./components/Theme";
+import { BrowserRouter } from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
